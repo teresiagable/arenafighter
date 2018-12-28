d8d65ebde2b3b88f90271c770c9e630534be3b8a
Teresia Gable <teresia.gable>
2018-12-28 16:25:15 +0100

    putta

--- src/main/java/se/lexicon/teresia/arena_fighter/Round.java
+++ src/main/java/se/lexicon/teresia/arena_fighter/Round.java
@@ -2,7 +2,6 @@ package se.lexicon.teresia.arena_fighter;
 
 public class Round {
 	
-
 	public Fighter striker;
 	public Fighter target;
 	public int hitValue;
@@ -12,16 +11,10 @@ public class Round {
 	public Round(Fighter player1, Fighter player2, Fighter striker, Fighter target, int hitValue, int targetHealth,
 			boolean targetSurvived) {
 
-		this.player1 = player1;
-		this.player2 = player2;
 		this.striker = striker;
 		this.target = target;
 		this.hitValue = hitValue;
 		this.targetHealth = targetHealth;
 		this.targetSurvived = targetSurvived;
 	}
-	
-	
-	
-
 }
