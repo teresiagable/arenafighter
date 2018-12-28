efa5efdd4f6ddbfd7d969101be6a02105e19fadd
Teresia Gable <teresia.gable@gmail.com>
2018-12-28 16:21:15 +0100

    master igen

--- src/main/java/se/lexicon/teresia/arena_fighter/Battle.java
+++ src/main/java/se/lexicon/teresia/arena_fighter/Battle.java
@@ -1,11 +1,21 @@
 package se.lexicon.teresia.arena_fighter;
 
+import java.util.Arrays;
+
 public class Battle {
-	public int id;
-	public Round[] rounds = new Round[1];
-	
-	
-	AddRound(){}
-	PrintBattleLog(){}
+	public Fighter player1;
+	public Fighter player2;
+	public Round[] rounds = new Round[0];
+
+	public Battle(Fighter player1, Fighter player2) {
+		this.player1 = player1;
+		this.player2 = player2;
+	}
+
+	public void AddRound(Round newRound) {
+		this.rounds = Arrays.copyOf(this.rounds, this.rounds.length + 1); // add one round for the new data
+		System.arraycopy(newRound, 0, this.rounds, this.rounds.length - 1, 1);// add round to Battle.rounds
+	}
+	// PrintBattleLog(){}
 
 }
--- src/main/java/se/lexicon/teresia/arena_fighter/Round.java
+++ src/main/java/se/lexicon/teresia/arena_fighter/Round.java
@@ -2,6 +2,25 @@ package se.lexicon.teresia.arena_fighter;
 
 public class Round {
 	
+
+	public Fighter striker;
+	public Fighter target;
+	public int hitValue;
+	public int targetHealth;
+	public boolean targetSurvived;
+	
+	public Round(Fighter player1, Fighter player2, Fighter striker, Fighter target, int hitValue, int targetHealth,
+			boolean targetSurvived) {
+
+		this.player1 = player1;
+		this.player2 = player2;
+		this.striker = striker;
+		this.target = target;
+		this.hitValue = hitValue;
+		this.targetHealth = targetHealth;
+		this.targetSurvived = targetSurvived;
+	}
+	
 	
 	
 
